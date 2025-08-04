7ea81a3db80acca89cee38555d4a1f4b6615b32e
kunhyung <kunhyung0823@khu.ac.kr>
2025-08-04 20:43:26 +0900

    first commit

---
+++ examples.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id VARCHAR(20) NOT NULL PRIMARY KEY,
+    password VARCHAR(20) NOT NULL
+);
+
+INSERT INTO users VALUES('1','1234@');
+INSERT INTO users VALUES('2','5678@');
+INSERT INTO users VALUES('3','91011@');
+
+
+CREATE TABLE pets(
+    age int(10) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    breed VARCHAR(20) NOT NULL,
+    id VARCHAR(20) NOT NULL,
+    FOREIGN KEY(id) REFERENCES users(id)
+);
+
+INSERT INTO pets VALUES (2,'apple','male','Golden Retriever','1');
+INSERT INTO pets VALUES (1,'banana','female','Border Collie','2');
+INSERT INTO pets VALUES (1,'cute','female','Maltese','3');
---
+++ pets.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const cors = require('cors');
+
+app.use(express.json())
+
+exports.pet_register = function(req, res){
+    const {age, name, gender, breed} = req.body;
+  if(!age || !name ||!gender || !breed ){
+    return res.status(400).json({success: false, message: '나이, 이름, 성별, 종을 입력해 주세요.'});
+  }
+
+    db.query(`SELECT * FROM pets WHERE name=?`,[name],(error, pet)=>{
+      if(error){
+        return res.status(500).json({success: false, message: '서버 에러'});
+      }
+      if(pet.length >0){
+        return res.status(400).json({success: false, message: '이미 존재하는 강아지 입니다.'});
+      }
+      db.query(`INSERT INTO pets(age, name, gender, breed) VALUES(?,?,?,?)`,[age, name, gender, breed],(error2, result)=>{
+        if(error2){
+          return res.status(500).json({success: false, message: '서버 에러'});
+        }
+        return res.json({success: true, message:'강아지 등록에 성공하였습니다.'});
+      });
+    }); 
+};
+
+exports.pet_update= function(req, res){
+  const {new_age, new_name, new_gender, new_breed} = req.body;
+  db.query(`UPDATE users SET age=?, name=?, gender=?, breed=? WHERE id=?`, 
+    [new_age, new_name, new_gender, new_breed, id], (error, result)=>{
+    if(error){
+      return res.status(500).json({success: false, message: '서버 에러'});
+    }
+    return res.status(200).json({success: true, message: '강아지 정보 저장되었습니다.'})
+  })
+}
