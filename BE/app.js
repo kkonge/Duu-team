8433cb80b88c45e91683cf60022a92dc2587d4ac
kunhyung08 <kunhyung0823@khu.ac.kr>
2025-08-05 14:14:48 +0900

    Add files via upload

---
+++ BE/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const cors = require('cors');
+const usersFunction = require('./users.js');
+const petsFunction = require('./pets.js');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/home', (req, res)=>{      //홈화면 어떻게 할지 프론트 팀이랑 상의
+  usersFunction.home(req, res);
+});
+
+app.post('/user_register', (req, res)=>{  //사용자 등록
+  usersFunction.user_register(req, res);
+});
+
+app.post('/pet_register', (req, res)=>{  //강아지 등록 
+  petsFunction.pet_register(req, res)
+});
+
+app.put('/user_update', (req, res)=>{   //사용자 정보 갱신 
+  usersFunction.user_update(req, res);
+});
+
+app.put('/pet_update', (req, res)=>{   //강아지 정보 갱신 
+  petsFunction.pet_update(req, res);
+});
+
+app.delete('/delete_user', (req, res)=>{  //사용자 정보 완전 삭제 
+  usersFunction.delete_user(req ,res);
+});
+
+app.delete('/delete_pet', (req, res)=>{ //강아지 정보 완전 삭제 
+  petsFunction.delete_pet(req, res);
+});
+
+app.listen(3000,()=>{
+  console.log('server is running on 3000 port');
+});
---
+++ BE/users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const cors = require('cors');
+
+app.use(express.json())
+
+
+exports.home = function(req, res){
+  try{
+    db.query(`SELECT * FROM users`, (error, users)=>{
+      if(error){
+        return res.status(500).json({success: false, message: '서버 에러'});
+      }
+      db.query(`SELECT * FROM pets`, (error2, pets)=>{
+        if(error2){
+          return res.status(500).json({success: false, message: '서버 에러'});
+        }
+        res.json({users, pets});
+      });
+    });
+  } catch(e){
+    return res.status(500).json({success: false, message: '서버 에러'});
+  }
+}
+
+exports.user_register = function(req, res){
+    const {id, password} = req.body;
+    
+    if(!id ||!password){
+        return res.status(400).json({success: false, message: '아이디와 비밀번호를 입력해 주세요.'});
+    }
+
+    db.query(`SELECT * FROM users WHERE id=?`,[id],(error, user)=>{
+      if(error){
+        return res.status(500).json({success: false, message: '서버 에러'});
+      }
+      if(user.length >0){
+        return res.status(400).json({success: false, message: '이미 존재하는 아이디 입니다.'});
+      }
+
+      db.query(`INSERT INTO users(id, password) VALUES(?,?)`,[id, password],(error2, result2)=>{
+        if(error2){
+          return res.status(500).json({success: false, message: '서버 에러'});
+        }
+        return res.json({success: true, message:'회원가입에 성공하였습니다.'});
+      });
+    }); 
+};
+
+exports.user_update= function(req, res){
+  const {new_id, new_password, id} = req.body;
+  db.query(`UPDATE users SET id=?, password=? WHERE id=?`, [new_id, new_password, id], (error, result)=>{
+    if(error){
+      return res.status(500).json({success: false, message: '서버 에러'});
+    }
+    return res.status(200).json({success: true, message: '사용자 정보 저장되었습니다.'})
+  })
+};
+
+exports.delete_user = function(req ,res){
+  const {id} = req.body;
+  db.query('DELETE FROM usres WHERE id=?', [id], (error, result)=>{
+    if(error){
+      return res.status(500).json({success: false, message: '데이터 베이스 오류'})
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ success: false, message: '올바른 아이디를 써주세요.' });
+    }
+  })
+};
