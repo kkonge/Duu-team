27bae2e2e239fc5ea49cdcc6df09b6557b13045e
ktjdus <3217sy@khu.ac.kr>
2025-08-06 15:57:05 +0900

    Pool 구조로 데이터베이스 연결 변경

--- BE/db.js
+++ BE/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host:'localhost',
   user:'root',
-  password:'kunhyung0823@',
-  database: 'application'
+  password:'', //각자 로컬 환경에 맞게 설정
+  database: 'duu', //각자 로컬 환경에 맞게 설정
+  waitForConnections: true,
+  connectionLimit: 10, //최대 연결 수
+  queueLimit: 0, //대기열 제한 없음
 });
-
-db.connect();
 
 module.exports = db;
